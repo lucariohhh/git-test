2e1d5b12ea7d3b65caa3b412164d82640da60687
Neil <Neil@github.com>
2023-06-09 10:17:19 +0800

    4th commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello gitee");
+        System.out.println("hello master");
     }
 }
