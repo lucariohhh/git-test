f934b2d11600f77daeecb9e102ad18a1d2510c79
Neil <Neil@github.com>
2023-06-09 11:44:58 +0800

    blue commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("hello gitee");
         System.out.println("hello master");
+        System.out.println("hello blue");
+
     }
 }
